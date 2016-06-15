b955afd078e492aa0fdf0a2d4c3f90c6daf602cf
Tim Currie <tcurrie@brightcove.com>
2016-06-15 18:19:50 -0400

    Updated some more things for query building

--- xeninfo.pl
+++ xeninfo.pl
@@ -10,6 +10,8 @@ use constant REPORT_HOSTS	=> 1;
 use constant REPORT_PACKAGES	=> 2;
 use constant REPORT_GUESTS	=> 3;
 
+my %Hosts;
+
 our $Limit = FALSE;
 our $List = FALSE;
 our $Report = REPORT_ALL;
@@ -17,7 +19,7 @@ our $Host = "";
 our $Package = "";
 our $Guest = "";
 our $Datacenter = "";
-our $Rebooted = FALSE;
+our $Rebooted = undef();
 
 our %ParamDefs = (
     "limit"	=> {
@@ -100,14 +102,7 @@ sub report_host {
 
     printf("Packages on host %s:\n", $host->{hostname});
 
-    my @packages;
-
-    my $query="SELECT packages.* FROM packages,installs WHERE packages.id=installs.package_id AND installs.host_id=$host->{id}";
-
-    $DB->get_hashref_array(\@packages, $query);
-    debugdump(DEBUG_DUMP, "packages", \@packages);
-
-    foreach my $package ( @packages ) {
+    foreach my $package ( @{$host->{packages}} ) {
         printf("  %s\n", $package->{name});
         last if $Limit;
     }
@@ -117,40 +112,33 @@ sub report_host {
 
 sub report_hosts {
 
-    my @hosts;
-    
-    my $query = sprintf("SELECT * FROM hosts WHERE rebooted=%s", $Rebooted?"true":"false");
-    $DB->get_hashref_array(\@hosts, $query);
-    debugdump(DEBUG_DUMP, "hosts", \@hosts);
+    printf("NOTE: Showing only hosts that have%s been rebooted.\n\n", $Rebooted?"":" NOT");
 
     if ( $List ) {
         printf("Hosts found:\n\n");
-        foreach my $host ( @hosts ) {
+        foreach my $host ( values(%Hosts) ) {
             printf("  %s\n", $host->{hostname});
         last if $Limit;
         }
     }
     elsif ( $Guest ne "" ) {
-        # List all the hosts that have a matching guest
-        $DB->get_hashref_array(\@hosts, "SELECT * FROM hosts,guests WHERE hosts.id=guests.host_id AND guests.guestname like '%$Guest%'");
-        debugdump(DEBUG_DUMP, "hosts", \@hosts);
         my $fmt = "%-24s\t%-24s\n";
         printf($fmt, "Guest", "Host");
         printf($fmt, "=" x 24, "=" x 24);
-        foreach my $host ( @hosts ) {
-            printf($fmt, $$host{guestname}, $$host{hostname});
+        foreach my $host ( values(%Hosts) ) {
+            printf($fmt, $host->{guestname}, $host->{hostname});
         }
     }
     else {
         printf("Packages by host:\n\n");
         if ( $Host eq "" ) {
-            foreach my $host ( @hosts ) {
+            foreach my $host ( values(%Hosts) ) {
                 report_host($host);
                 last if $Limit;
             }
         }
         else {
-            report_host(grep({$_->{hostname} =~ $Host} @hosts));
+            report_host(grep({$_->{hostname} =~ $Host} values(%Hosts)));
         }
     }
 }
@@ -218,59 +206,131 @@ sub report_guest {
 
 sub report_guests {
 
-    my @guests;
-
-    my $query = sprintf("SELECT DISTINCT guests.*,hosts.hostname FROM guests,hosts WHERE hosts.id=guests.host_id AND hosts.rebooted=%s",
-        $Rebooted?"true":"false");
-
-    $DB->get_hashref_array(\@guests, $query);
-
-    debugdump(DEBUG_DUMP, "guests", \@guests);
-
     if ( $List ) {
         printf("Guests found:\n\n");
-        foreach my $guest ( @guests ) {
-            printf("  %s\n", $guest->{guestname});
-            last if $Limit;
-        }
-    }
-    else {
-        printf("Guests by host:\n\n");
-        if ( $Host eq "" ) {
-            my @hosts;
-            my $query = sprintf("SELECT * FROM hosts WHERE rebooted=%s", $Rebooted?"true":"false");
-            $DB->get_hashref_array(\@hosts, $query);
-            foreach my $host ( @hosts ) {
-                printf("%s:\n", $host->{hostname});
-                foreach my $guest ( grep({$_->{hostname} eq $host->{hostname}} @guests) ) {
-                    printf("  %s\n", $guest->{guestname});
-                    last if $Limit;
-                }
-                printf("\n");
-                last if $Limit;
-            }
-        }
-        else {
-            printf("%s:\n", $Host);
-            foreach my $guest ( grep({$_->{hostname} =~ $Host} @guests) ) {
+        foreach my $host ( values(%Hosts) ) {
+            foreach my $guest ( @{$host->{guests}} ) {
                 printf("  %s\n", $guest->{guestname});
                 last if $Limit;
             }
         }
     }
+    else {
+        printf("Guests by host:\n\n");
+
+        if ( $Host eq "" ) {
+
+            foreach my $host ( values(%Hosts) ) {
+
+                printf("%s:\n", $host->{hostname});
+
+                foreach my $guest ( @{$host->{guests}} ) {
+                    printf("  %s\n", $guest->{guestname});
+                    last if $Limit;
+                }
+
+                printf("\n");
+
+                last if $Limit;
+            }
+        }
+        else {
+
+            if ( exists($Hosts{$Host}) ) {
+
+                printf("%s:\n", $Host);
+
+                foreach my $guest ( @{$Hosts{$Host}{guests}} ) {
+                    printf("  %s\n", $guest->{guestname});
+                    last if $Limit;
+                }
+            }
+            else {
+                debugprint(DEBUG_WARN, "Host not found: '%s'", $Host);
+            }
+        }
+    }
+}
+
+sub gather_data
+{
+    debugprint(DEBUG_TRACE, "Gathering host, guest, and package information...");
+
+    my @hosts;
+    my $queryspec = {
+        action	=> ACTION_SELECT,
+        select	=> "hosts.*",
+        join	=> "hosts",
+        order	=> "hostname",
+    };
+
+    push(@{$queryspec->{where}}, "rebooted='$Rebooted'") if defined($Rebooted);
+    push(@{$queryspec->{where}}, "datacenter='$Datacenter'") if defined($Datacenter);
+
+    my $query = $DB->query($queryspec);
+    $DB->get_hashref_array(\@hosts, $query);
+    map({$Hosts{$_->{hostname}} = $_} @hosts);
+
+    foreach my $host ( @hosts ) {
+
+        # Gather data about guests on this host...
+        my $guests = [];
+
+        $queryspec = {
+            action	=> ACTION_SELECT,
+            select	=> "*",
+            join	=> "guests",
+            where	=> [
+                "guests.host_id=$host->{id}"
+            ]
+        };
+
+        $query = $DB->query($queryspec);
+        $DB->get_hashref_array($guests, $query);
+        debugdump(DEBUG_DUMP, "guests", $guests);
+
+        $Hosts{$host->{hostname}}{guests} = $guests;
+
+        # Gather data on packages installed on this host...
+        my $packages = [];
+
+        $queryspec = {
+            action	=> ACTION_SELECT,
+            select	=> "packages.*",
+            join	=> "packages,installs",
+            where	=> [
+                "packages.id=installs.package_id",
+                "installs.host_id=$host->{id}",
+            ],
+        };
+
+        $query = $DB->query($queryspec);
+        $DB->get_hashref_array($packages, $query);
+        debugdump(DEBUG_DUMP, "packages", $packages);
+
+        $Hosts{$host->{hostname}}{packages} = $packages;
+
+        last if $Limit;
+    }
+
+    debugdump(DEBUG_DUMP, "Hosts", \%Hosts);
 }
 
 # Main Program
 {
     # Set up...
     register_params(\%ParamDefs);
+
     parse_args();
+
+    gather_data();
 
     printf("Xen Server Package Inventory\n\n");
 
     if ( $Report == REPORT_ALL ) {
         report_hosts();
         report_packages();
+        report_guests();
     }
     elsif ( $Report == REPORT_HOSTS ) {
         report_hosts();
